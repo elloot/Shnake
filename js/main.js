77fb216d981a3c43e4e551274c1be1146ec748d7
elloot <elliot.duchek@gmail.com>
2020-05-13 20:41:40 +0200

    refactor lots of code by making a Game class

--- js/main.js
+++ js/main.js
@@ -1,9 +1,6 @@
 class Snake {
-    constructor(size) {
-        this.size = size;
-
-        console.log(gameboard.leftBound + this.size, gameboard.topBound + this.size);
-        this.firstBlock = new SnakeBlock(this.size, this.size, this.size);
+    constructor() {
+        this.firstBlock = new SnakeBlock(game.blockSize, game.blockSize, game.blockSize);
         this.lastBlock = this.firstBlock;
 
         gameboard.removeAvailableSpot(this.firstBlock.x, this.firstBlock.y);
@@ -11,13 +8,13 @@ class Snake {
     addBlock() {
         this.lastBlockSave = this.lastBlock;
 
-        this.lastBlock = new SnakeBlock(this.lastBlockSave.x, this.lastBlockSave.y, this.size);
+        this.lastBlock = new SnakeBlock(this.lastBlockSave.x, this.lastBlockSave.y, game.blockSize);
 
         this.lastBlockSave.blockBehind = this.lastBlock;
     }
     update() {
         this.firstBlock.move();
-        document.querySelector(".score").innerHTML = score;
+        document.querySelector(".score").innerHTML = game.score;
     }
     setXDirection(vx) {
         this.firstBlock.vx = vx;
@@ -29,10 +26,30 @@ class Snake {
     }
 }
 
+class Game {
+    constructor(size, interval) {
+        this.blockSize = size;
+        this.updateInterval = interval;
+        this.score = 0;
+        this.running;
+    }
+
+    start() {
+        this.running = true;
+        window.requestAnimationFrame(runSnake);
+        apple.place();
+    }
+
+    end() {
+        this.running = false;
+        document.querySelector(".ULOST").style.display = "block";
+    }
+}
+
 class Gameboard {
     constructor(clientHeight, clientWidth) {
-        this.width = round(clientWidth, 50);
-        this.height = round(clientHeight, 50);
+        this.width = round(clientWidth, game.blockSize);
+        this.height = round(clientHeight, game.blockSize);
 
         this.element = document.querySelector(".gameboard");
 
@@ -44,8 +61,8 @@ class Gameboard {
 
         this.availableSpots = [];
 
-        for (let y = 0; y < this.height; y += 50) {
-            for (let x = 0; x < this.width; x += 50) {
+        for (let y = 0; y < this.height; y += game.blockSize) {
+            for (let x = 0; x < this.width; x += game.blockSize) {
                 this.addAvailableSpot(x, y);
             }
         }
@@ -67,7 +84,7 @@ class Gameboard {
 
 class Apple {
     constructor() {
-        this.size = snake.size;
+        this.blockSize = game.blockSize;
     }
 
     place() {
@@ -79,8 +96,8 @@ class Apple {
         this.x = gameboard.availableSpots[spotIndex].x;
         this.y = gameboard.availableSpots[spotIndex].y;
 
-        this.element.style.width = this.size + "px";
-        this.element.style.height = this.size + "px";
+        this.element.style.width = this.blockSize + "px";
+        this.element.style.height = this.blockSize + "px";
 
         this.element.addEventListener("animationend", (e) => {
             if (e.animationName === "apple-disappear") {
@@ -97,7 +114,7 @@ class Apple {
 
 class SnakeBlock {
     constructor(x, y, size) {
-        this.size = size;
+        this.blockSize = size;
         this.x = x;
         this.y = y;
 
@@ -109,8 +126,8 @@ class SnakeBlock {
         this.element = document.createElement("div");
         this.element.classList.add("snake-block", "block");
 
-        this.element.style.width = this.size + "px";
-        this.element.style.height = this.size + "px";
+        this.element.style.width = this.blockSize + "px";
+        this.element.style.height = this.blockSize + "px";
 
         this.element.style.left = this.x + "px";
         this.element.style.top = this.y + "px";
@@ -134,14 +151,14 @@ class SnakeBlock {
             this.y = frontY;
         } else {
             //game over detector
-            if (this.x + this.vx * this.size >= gameboard.width || this.x + this.vx * this.size < 0 || this.y + this.vy * this.size < 0 || this.y + this.vy * this.size >= gameboard.height || isSelfcolliding(this.blockBehind, this)) {
-                endGame();
+            if (this.x + this.vx * this.blockSize >= gameboard.width || this.x + this.vx * this.blockSize < 0 || this.y + this.vy * this.blockSize < 0 || this.y + this.vy * this.blockSize >= gameboard.height || isSelfcolliding(this.blockBehind, this)) {
+                game.end();
                 return;
             }
 
             //moves block
-            this.x += this.vx * this.size;
-            this.y += this.vy * this.size;
+            this.x += this.vx * this.blockSize;
+            this.y += this.vy * this.blockSize;
         }
 
         gameboard.removeAvailableSpot(this.x, this.y);
@@ -152,7 +169,7 @@ class SnakeBlock {
 
         //applpe chonk detector
         if (this.x === apple.x && this.y === apple.y) {
-            score++;
+            game.score++;
             snake.addBlock();
             apple.element.classList.add("apple-disappear");
             apple.place();
@@ -165,18 +182,13 @@ class SnakeBlock {
     }
 }
 
-let gameboard = new Gameboard(document.body.clientHeight, document.body.clientWidth);
+const game = new Game(parseInt(window.prompt("HOW THICC 🍑 SHOULD SHNAKE BE? WE RECMND 50", 50)), parseInt(window.prompt("HOW QUICK U WANT GAME? WE RECMND 150", 150)));
 
-const snake = new Snake(parseFloat(window.prompt("HOW THICC 🍑 SHOULD SHNAKE BE? WE RECMND 50", 50)));
+const gameboard = new Gameboard(document.body.clientHeight, document.body.clientWidth);
 
-const updateInterval = parseFloat(window.prompt("HOW QUICK U WANT GAME? WE RECMND 150", 150));
+const snake = new Snake();
 
-let apple = new Apple();
-apple.place();
-
-let score = 0;
-
-let running;
+const apple = new Apple();
 
 let lastTime = new Date().getTime();
 let deltaUpdate = 0;
@@ -184,8 +196,7 @@ let deltaUpdate = 0;
 window.addEventListener(
     "load",
     () => {
-        running = true;
-        window.requestAnimationFrame(runSnake);
+        game.start();
 
         document.addEventListener("keydown", (e) => {
             switch (e.key) {
@@ -209,9 +220,6 @@ window.addEventListener(
 
                     snake.setYDirection(1);
                     break;
-                case "Alt":
-                    for (i = 0; i < 100; i++) apple.place();
-                    break;
             }
         });
     },
@@ -228,21 +236,16 @@ function round(toRound, roundTo) {
 
 function isSelfcolliding(currentBlock, foremostBlock) {
     while (currentBlock) {
-        if (currentBlock.x === foremostBlock.x + foremostBlock.vx * foremostBlock.size && currentBlock.y === foremostBlock.y + foremostBlock.vy * foremostBlock.size) {
+        if (currentBlock.x === foremostBlock.x + foremostBlock.vx * foremostBlock.blockSize && currentBlock.y === foremostBlock.y + foremostBlock.vy * foremostBlock.blockSize) {
             return true;
         }
         currentBlock = currentBlock.blockBehind;
     }
 }
 
-function endGame() {
-    running = false;
-    document.querySelector(".ULOST").style.display = "block";
-}
-
 function runSnake() {
     let currentTime = new Date().getTime();
-    deltaUpdate += (currentTime - lastTime) / updateInterval;
+    deltaUpdate += (currentTime - lastTime) / game.updateInterval;
     lastTime = currentTime;
 
     while (deltaUpdate >= 1) {
@@ -250,7 +253,7 @@ function runSnake() {
         snake.update();
     }
 
-    if (running) {
+    if (game.running) {
         window.requestAnimationFrame(runSnake);
     }
 }
